f338e54022f363fa26c38214cded65c8b6863882
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-04-08 09:27:20 +0100

    correction workshop1-version Thomas

---
+++ exemple.js
@@ -0,0 +1,9 @@
+// declaration
+function info() {
+    for (var x = 0; x < 10; x++) {
+        console.log("x = " + x);
+    }
+    console.log("final val de x = " + x);
+}
+// appel de la fonction
+info();
---
+++ exemple.ts
@@ -0,0 +1,15 @@
+// declaration
+
+function info()
+{
+    for(let x = 0; x<10;x++)
+    {
+        console.log("x = "+x);
+    }
+
+    console.log("final val de x = "+x);
+}
+
+
+// appel de la fonction
+info();
